412de8a24a38794b24cda11e84ec63a8b2526377
AvrilC9 <avrilc8@nycstudents.net>
2024-09-10 17:33:46 -0400

    Lab 1

---
+++ Cao_Avril_Lab_1.cpp
@@ -0,0 +1,48 @@
+// Avril Cao//
+//lab 1//
+//Septemer 10, 2024//
+
+#include <iostream>
+
+// #include <iostream> allows cout to generate output and endl
+
+using namespace std;
+
+// using namespace std; allowa cout and endl to be used without the prefix std::
+
+int main()
+
+// the function main is needed in all C++ programs
+
+{
+
+// I used cout to put in the output and I put endl; in the end when starting a next line
+
+    cout << "#######################################################################################" << endl;
+    cout << "#                                                                                     #" << endl;                                                                                            
+    cout << "#  Ways to access the Task Manager on your Windows computer:                          #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#          Press the key combination Ctrl + Shift + Escape                            #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#          Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#          Type “Task Manager” in the Windows Start menu search                       #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#######################################################################################" << endl; 
+
+// I created a box of hashtags around the steps using the output statement cout
+
+    return 0;
+}
+
+// #######################################################################################
+// #                                                                                     #
+// #  Ways to access the Task Manager on your Windows computer:                          #
+// #                                                                                     #
+// #          Press the key combination Ctrl + Shift + Escape                            #
+// #                                                                                     #
+// #          Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #
+// #                                                                                     #
+// #          Type “Task Manager” in the Windows Start menu search                       #
+// #                                                                                     #
+// #######################################################################################
